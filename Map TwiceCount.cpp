97189f37be9fa44aa8186e939d78aeda834da375
Dhanonjoy-2021331099 <138089152+Dhanonjoy-2021331099@users.noreply.github.com>
2024-02-26 02:31:08 +0600

    Update Map TwiceCount.cpp

--- Map TwiceCount.cpp
+++ Map TwiceCount.cpp
@@ -1,1 +1,50 @@
 
+// https://www.geeksforgeeks.org/problems/twice-counter4236/1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int t; cin >> t ;
+   while(t--){
+       int n; cin >> n;
+       map<string,int> cnt;
+       for(int i=0; i<n;i++){
+           string s; cin >> s;
+           cnt[s]++;
+       }
+       int ans = 0;
+       
+       for(auto u : cnt) {
+           if(u.second ==2) ans ++;
+       }
+       cout << ans <<endl;
+       /*
+       for(auto u : cnt) {
+           if(u.second == 2) cout << u.first << endl;
+       }
+       */
+   }
+    
+    return 0;
+}
+
+
+
+/*
+Input:
+N = 3
+list = {Geeks, For, Geeks}
+Output: 1
+Explanation: 'Geeks' is the only word that 
+appears twice. 
+
+Input:
+N = 8
+list = {Tom, Jerry, Thomas, Tom, Jerry, 
+Courage, Tom, Courage}
+Output: 2
+Explanation: 'Jerry' and 'Courage' are the 
+only words that appears twice. 
+
+*/
