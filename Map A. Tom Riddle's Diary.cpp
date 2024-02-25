7eb6db00a876bf8314240747134cca50792f5a56
Dhanonjoy-2021331099 <138089152+Dhanonjoy-2021331099@users.noreply.github.com>
2024-02-26 02:22:41 +0600

    Update Map A. Tom Riddle's Diary.cpp

--- Map A. Tom Riddle's Diary.cpp
+++ Map A. Tom Riddle's Diary.cpp
@@ -1,1 +1,19 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    map<string,bool> vis;
+    while(n--){
+        string s; cin >> s;
+        
+        if(vis[s]==1) cout << "YES"<<endl;
+        else cout << "NO"<<endl;
+        
+        vis[s] =1;
+        
+    }
+ 
+    return 0;
+}
