e154649d190de1ab82df94d8940a3ef652aae543
Dhanonjoy-2021331099 <138089152+Dhanonjoy-2021331099@users.noreply.github.com>
2024-02-26 02:28:40 +0600

    Update Map B. Radio Station.cpp

--- Map B. Radio Station.cpp
+++ Map B. Radio Station.cpp
@@ -1,1 +1,30 @@
+// https://codeforces.com/contest/918/problem/B
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    
+    int n, m;
+    cin >> n >> m;
+    
+    map<string,string> ipName;
+    
+    for(int i=0; i<n;i++){
+        string name,ip;
+        cin >> name >> ip;
+        ipName[ip] = name;
+        
+    }
+    
+    while(m--){
+        string s1,ip;
+        cin >> s1 >> ip;
+        ip.pop_back();
+        
+        cout << s1 << " "<<ip << "; #"<<ipName[ip] <<endl;
+    }
+    
+    
+ 
+    return 0;
+}
