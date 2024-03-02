e8b32e1e21b2d28ecbe8d902ec7c183b360e4833
Dhanonjoy-2021331099 <138089152+Dhanonjoy-2021331099@users.noreply.github.com>
2024-03-02 10:31:00 +0600

    Add files via upload

---
+++ String_LexoGeek.cpp
@@ -0,0 +1,14 @@
+// https://www.hackerearth.com/problem/algorithm/lexogeek-e6450fb7/
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int t; cin >> t;
+	
+	while(t--){
+		string s; cin >> s;
+		if(next_permutation(s.begin(),s.end()))
+		cout << s <<endl;
+	else cout << "no answer"<<endl;
+	}
+	
+}
