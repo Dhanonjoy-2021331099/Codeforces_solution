31ab229ad244ae248a6f2c76619d4fa44f8cc5a4
Dhanonjoy-2021331099 <138089152+Dhanonjoy-2021331099@users.noreply.github.com>
2024-03-02 10:27:25 +0600

    Add files via upload

---
+++ String_Haiku.cpp
@@ -0,0 +1,32 @@
+// https://codeforces.com/contest/78/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isVowel(char c){
+    return (c=='a'||c=='e'||c=='i'||c=='o'||c=='u');
+}
+int main() {
+    vector<string>v;
+    int cnt[] ={5,7,5};
+    
+    for(int i=0; i<3;i++){
+        string s;
+        char c;
+        cin >> c;
+        getline(cin , s);
+        s = c+s;
+        
+        v.push_back(s);
+    }
+    
+    for(int i =0; i<3;i++){
+        int c = 0;
+        for(auto u : v[i]){
+            if(isVowel(u)) c++;
+        }
+        if(c != cnt[i]) return cout <<"NO"<<endl, 0;
+    }
+    cout << "YES"<<endl;
+    
+    
+}
